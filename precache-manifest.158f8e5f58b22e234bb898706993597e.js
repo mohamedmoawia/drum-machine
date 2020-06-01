915e6bbd359fc76e50be6511bc6646b0b7adcf19
mohamedmoawia <mo3wia94@gmail.com>
2020-06-01 13:50:00 +0200

    Updates

---
+++ precache-manifest.158f8e5f58b22e234bb898706993597e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2afabdd97719172ffba88a3a874cb9c",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "9531252502afb4d9bd71",
+    "url": "/drum-machine/static/css/main.82ff5a22.chunk.css"
+  },
+  {
+    "revision": "1bf55eceebd23fdd7759",
+    "url": "/drum-machine/static/js/2.8a9a138b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum-machine/static/js/2.8a9a138b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9531252502afb4d9bd71",
+    "url": "/drum-machine/static/js/main.b124e160.chunk.js"
+  },
+  {
+    "revision": "e426264f021e369657ac",
+    "url": "/drum-machine/static/js/runtime-main.5bfd1e01.js"
+  }
+]);
